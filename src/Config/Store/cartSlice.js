9597c94511acb76f2fb2fa5fa7efa34f34013130
Ashfaq Ahmed <135797310+Ashfaqahmed05@users.noreply.github.com>
2024-03-14 19:30:16 +0500

    Add files via upload

--- src/Config/Store/cartSlice.js
+++ src/Config/Store/cartSlice.js
@@ -1,19 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit'
+
 const cartSlice = createSlice({
   name: 'cart',
   initialState: {
-      cart: []
+    cart: []
   },
   reducers: {
-      updateCart: (state, action) => {
-          state.cart.push(action.payload)
-
-          console.log('data', action)
-      },
-      removeFromCart: (state, action) => {
-        state.cart = state.cart.filter(item => item.id !== action.payload.id);
-        console.log(action.payload.id)
-      }
+    updateCart: (state, action) => {
+      state.cart.push(action.payload)
+      console.log('data', action)
+    },
+    removeFromCart: (state, action) => {
+      state.cart = state.cart.filter(item => item.id !== action.payload.id)
+      console.log('data removed:', action.payload)
+      return state
+    }
   }
 })
 
